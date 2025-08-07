2f45e76121da15d947de1b2fbfbcf38fe7c9f06f
Meeman <meeman_3@outlook.com>
2025-08-07 19:51:09 +0100

    added printed report

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from stats import word_count
-from stats import character_count
+from stats import word_count, character_count, sort_dict, list_dict
 
 def get_books_text(file_path):
     
@@ -13,12 +12,20 @@ def main():
 
     book_words = get_books_text("books/frankenstein.txt")
 
+    num_words = word_count(book_words)
+
     book_characters = character_count(book_words)
 
-    print(f"{word_count(book_words)} words found in the document")
+    d_list_sort = list_dict(book_characters)
 
-    print(book_characters)
-
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for i in range(0, len(d_list_sort)):
+        print (f"{d_list_sort[i]["char"]}: {d_list_sort[i]["num"]}")
+    print("============= END ===============")
 
 
 
--- stats.py
+++ stats.py
@@ -1,4 +1,5 @@
-def word_count(book_words):
+#function that takes string of word and returns the number of words
+def word_count(book_words): 
 
     word_list = book_words.split()
 
@@ -6,7 +7,8 @@ def word_count(book_words):
 
     return num_words
 
-def character_count(book_words):
+#function that takes string of words and returns number of each character
+def character_count(book_words): 
 
     count_dict = {}
 
@@ -17,7 +19,35 @@ def character_count(book_words):
                 count_dict[f] += 1
             else:
                  count_dict[f] = 1
-                 
+
     return count_dict
+
+#function to sort by with .sort
+def sort_dict(dict):
+     return dict["num"]
+
+#function that takes dictionary of character and counts and returns sorted list of dictionaries
+def list_dict(count_dict):
+     
+    d_list = []
+
+    for c in count_dict:
+          
+        indi_dict = {"char": c, "num": count_dict[c]}
+          
+        d_list.append(indi_dict)
+    
+
+    d_list.sort(reverse=True, key=sort_dict)
+
+    
+    return d_list
+
+
+
+
+
+        
+    
 
 
